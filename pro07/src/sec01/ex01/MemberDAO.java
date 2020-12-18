00cd89499d9ea781cacd69713554bfd5342569be
rlozigu <rlozigu@gmail.com>
2020-12-18 23:41:29 +0900

    서블릿의 DB연동을 통한 회원 정보 조회

---
+++ pro07/src/sec01/ex01/MemberDAO.java
@@ -0,0 +1,64 @@
+package sec01.ex01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MemberDAO {
+	private static final String driver = "oracle.jdbc.driver.OracleDriver";
+	private static final String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+	private static final String user = "JAVAWEB";
+	private static final String pwd = "javaweb";
+	private Statement stmt;
+	private Connection con;
+	
+	public List<MemberVO> listMembers(){
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		try {
+			//네 가지 정보로 데이터베이스를 연결
+			connDB();
+			String query = "SELECT * FROM T_MEMBER";
+			System.out.println(query);
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				String id = rs.getString("id");
+				String pwd = rs.getString("pwd");
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				Date joinDate = rs.getDate("joinDate");
+				
+				MemberVO vo = new MemberVO();
+				vo.setId(id);
+				vo.setPwd(pwd);
+				vo.setName(name);
+				vo.setEmail(email);
+				vo.setJoinDate(joinDate);
+				list.add(vo);
+			}
+			rs.close();
+			stmt.close();
+			con.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	private void connDB() {
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle 드라이버 로딩 성공");
+			con = DriverManager.getConnection(url, user, pwd);
+			System.out.println("Connection 생성 성공");
+			stmt = con.createStatement();
+			System.out.println("Statement 생성 성공");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ pro07/src/sec01/ex01/MemberServlet.java
@@ -0,0 +1,58 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member")
+public class MemberServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> list = dao.listMembers();
+		
+		out.print("<html>");
+		out.print("<body>");
+		out.print("<table border = 1>");
+		out.print("<tr align = 'center' bgcolor ='lightgreen'>");
+		out.print("<td>아이디</td>");
+		out.print("<td>비밀번호</td>");
+		out.print("<td>이름</td>");
+		out.print("<td>이메일</td>");
+		out.print("<td>가입일</td>");
+		out.print("</tr>");
+		
+		for(int i = 0 ; i < list.size(); i++) {
+			MemberVO memberVO = list.get(i);
+			String id = memberVO.getId();
+			String pwd = memberVO.getPwd();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			
+			out.print("<tr>");
+			out.print("<td>"+id+"</td>");
+			out.print("<td>"+pwd+"</td>");
+			out.print("<td>"+name+"</td>");
+			out.print("<td>"+email+"</td>");
+			out.print("<td>"+joinDate+"</td>");
+			out.print("<tr>");
+		}
+		
+		out.print("</body>");
+		out.print("</html>");
+	}
+	
+	
+
+}
